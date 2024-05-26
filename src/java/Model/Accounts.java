24824cd6b3f69ac5a1b32fd16d46de0f8c7a963a
LINHNTHE170290 <linhnthe170290@fpt.edu.vn>
2024-05-26 22:32:03 +0700

    Update Accounts.java
    create Accounts model

--- src/java/Model/Accounts.java
+++ src/java/Model/Accounts.java
@@ -4,10 +4,128 @@
  */
 package Model;
 
+import java.sql.Timestamp;
+
 /**
  *
  * @author phamt
  */
 public class Accounts {
-    
+    private int id;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private String avatar;
+    private Boolean isMale;
+    private int roleId;
+    private Boolean isActive;
+    private Timestamp createAt;
+    private Timestamp updatedAt;
+    public Accounts() {
+    }
+
+    public Accounts(int id, String username, String password, String fullName, String email, String avatar, Boolean isMale, int roleId, Boolean isActive, Timestamp createAt, Timestamp updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.avatar = avatar;
+        this.isMale = isMale;
+        this.roleId = roleId;
+        this.isActive = isActive;
+        this.createAt = createAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Boolean getIsMale() {
+        return isMale;
+    }
+
+    public void setIsMale(Boolean isMale) {
+        this.isMale = isMale;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public Timestamp getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Timestamp createAt) {
+        this.createAt = createAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+  
 }
