065138a81c83be2198501140121a445cbd319d2a
LINHNTHE170290 <linhnthe170290@fpt.edu.vn>
2024-05-26 22:45:01 +0700

    Create LoginController.java
    This method performs the login process: receiving information from the user, validating the information, responding with errors if the information is incorrect, and creating a session and redirecting the user if the login is successful.

---
+++ src/java/Controller/LoginController.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import Dal.DAOAccount;
+import Model.Accounts;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author admin
+ */
+public class LoginController extends HttpServlet {
+
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String accountname = request.getParameter("username");
+        String password = request.getParameter("password");
+        DAOAccount da = new DAOAccount();
+        Accounts a = da.checkAuthentic(accountname, password);
+        if(a == null){
+            request.setAttribute("error", "Username or password incorrect!!!");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }else{
+            int sessionTimeoutSeconds = 2400;
+            //tao session
+            HttpSession session = request.getSession();
+            session.setAttribute("account", a);
+            response.sendRedirect("homepage");
+        }
+    }
+
+    
+
+}
