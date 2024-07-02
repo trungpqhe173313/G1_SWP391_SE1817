5779b8693bf8b92fb4e584b14927c0badee671bb
trungpqhe173313 <trungpqhe173313@fpt.edu.vn>
2024-07-02 17:05:49 +0700

    update iter1

--- src/java/Dal/OrderDAO.java
+++ src/java/Dal/OrderDAO.java
@@ -24,9 +24,47 @@ public class OrderDAO extends DBContext {
 
     public Order getOrderByAId(int customerId) {
         try {
-            String sql = "SELECT *\n"
-                    + "  FROM [Barber].[dbo].[Orders]\n"
-                    + "  where [Orders].customerId = ? and [Orders].statusID = 1";
+            String sql = "WITH OrderedShifts AS (\n"
+                    + "    SELECT \n"
+                    + "        o.orderId,\n"
+                    + "        o.orderCode,\n"
+                    + "        o.customerId,\n"
+                    + "        o.employeeId,\n"
+                    + "        o.statusID,\n"
+                    + "        o.orderDate,\n"
+                    + "        o.totalAmount,\n"
+                    + "        o.updateTime,\n"
+                    + "        s.id AS shiftId,\n"
+                    + "        s.startTime,\n"
+                    + "        ROW_NUMBER() OVER (PARTITION BY o.orderId ORDER BY s.startTime) AS rn\n"
+                    + "    FROM \n"
+                    + "        Orders o\n"
+                    + "    JOIN \n"
+                    + "        Order_shift os ON o.orderId = os.OrderID\n"
+                    + "    JOIN \n"
+                    + "        shift s ON os.ShiftID = s.id\n"
+                    + "    JOIN\n"
+                    + "        customer c ON o.customerId = c.customerId\n"
+                    + "    WHERE\n"
+                    + "        o.customerId = ? and  o.statusID = 1\n"
+                    + ")\n"
+                    + "SELECT \n"
+                    + "    orderId,\n"
+                    + "    orderCode,\n"
+                    + "    customerId,\n"
+                    + "    employeeId,\n"
+                    + "    statusID,\n"
+                    + "    orderDate,\n"
+                    + "    totalAmount,\n"
+                    + "    updateTime,\n"
+                    + "    shiftId,\n"
+                    + "    startTime\n"
+                    + "FROM \n"
+                    + "    OrderedShifts\n"
+                    + "WHERE \n"
+                    + "    rn = 1\n"
+                    + "ORDER BY \n"
+                    + "    orderId;";
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setInt(1, customerId);
             ResultSet rs = stm.executeQuery();
@@ -40,6 +78,10 @@ public class OrderDAO extends DBContext {
                 order.setOrderDate(rs.getDate(6));
                 order.setTotalAmount(rs.getInt(7));
                 order.setUpdateTime(rs.getString(8));
+                Shift shift = new Shift();
+                shift.setId(rs.getInt(9));
+                shift.setStartTime(rs.getString(10));
+                order.setShift(shift);
                 return order;
             }
         } catch (SQLException ex) {
@@ -123,19 +165,16 @@ public class OrderDAO extends DBContext {
         }
     }
 
-    public void upDateOrder(String date, String shift, String oId, int total) {
+    public void upDateOrder(String date, String oId, int total) {
         try {
             String sql = "UPDATE [dbo].[Orders]\n"
                     + "   SET [orderDate] = ?\n"
                     + "      ,[totalAmount] = ?\n"
-                    + "      ,[shiftId] = ?\n"
-                    + "      \n"
-                    + " WHERE [Orders].orderId = ?";
+                    + " WHERE [customerId] = ?";
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setString(1, date);
             stm.setInt(2, total);
-            stm.setString(3, shift);
-            stm.setString(4, oId);
+            stm.setString(3, oId);
             stm.executeUpdate();
         } catch (SQLException ex) {
             Logger.getLogger(OrderDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -245,8 +284,9 @@ public class OrderDAO extends DBContext {
         }
     }
 
-//    public static void main(String[] args) {
-//        OrderDAO o = new OrderDAO();
-//        o.upDateOrderAdmin("", "2", "7", 140000);
-//    }
+    public static void main(String[] args) {
+        OrderDAO o = new OrderDAO();
+        Order k = o.getOrderByAId(8);
+        System.out.println(k.getShift().getStartTime());
+    }
 }
